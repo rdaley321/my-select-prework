80c87f142be523a8e0a694ba62c9162611335b9a
Ryan Daley <rdaley321@yahoo.com>
2018-11-01 05:59:58 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i=0
   new_array = []
     while i < collection.length
-      yield(collection[i]) ? new_array <<
+      yield(collection[i]) ? new_array << collection[i] : nil
       i+=1
     end
 end
